f357e7a5f0a3d710eeccb518741710e550f84c97
David <davbetm@gmail.com>
2019-01-28 08:41:40 -0600

    1: Comunicación entre cliente y servidor mediante sockets

--- SocketsFlujo/src/Ejercicio1/Cliente.java
+++ SocketsFlujo/src/Ejercicio1/Cliente.java
@@ -1,9 +1,71 @@
 package Ejercicio1;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 /**
  *
  * @author david
  */
 public class Cliente {
+    private String ip = "127.0.0.1";
+    private int puerto = 5000;
+    private DataInputStream entrada;
+    private DataOutputStream salida;
+    private Socket cliente;
     
+    public Cliente() {
+        try {
+            cliente = new Socket(ip, puerto);
+            salida = new DataOutputStream(cliente.getOutputStream());
+            // escribir mensaje al servidor
+            salida.writeUTF("How are you? | Conexión exitosa.");
+            
+            // recibir mensaje del servidor
+            entrada = new DataInputStream(cliente.getInputStream());
+            System.out.println("Servidor dice >> " + entrada.readUTF());
+            
+            cliente.close();
+        }
+        catch(UnknownHostException enet) {
+            enet.printStackTrace();
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public Cliente(String mensaje) {
+        try {
+            cliente = new Socket(ip, puerto);
+            salida = new DataOutputStream(cliente.getOutputStream());
+            // escribir mensaje al servidor
+            salida.writeUTF(mensaje);
+            
+            // recibir mensaje del servidor
+            entrada = new DataInputStream(cliente.getInputStream());
+            System.out.println("Servidor dice >> " + entrada.readUTF());
+            
+            cliente.close();
+        }
+        catch(UnknownHostException enet) {
+            enet.printStackTrace();
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    
+    public static void main(String args[]) {
+        // Cliente c = new Cliente();
+        // 5 clientes se van a conectar con mensajes diferentes
+        String mensajes[] = new String[]{"hola", "hola2", "hola3", "hola4", "hola5"};
+        Cliente c;
+        for (int i = 0; i < 5; i++) {
+            c = new Cliente(mensajes[i]);
+        }
+    }
 }
--- SocketsFlujo/src/Ejercicio1/Servidor.java
+++ SocketsFlujo/src/Ejercicio1/Servidor.java
@@ -11,7 +11,7 @@ import java.net.Socket;
  * @author david
  */
 public class Servidor {
-    private int puerto = 21;
+    private int puerto = 5000;
     private ServerSocket servidor;
     private Socket cliente;
     private DataInputStream entrada;
@@ -21,14 +21,29 @@ public class Servidor {
         try {
             // El servidor ya está a la escucha
             servidor = new ServerSocket(puerto);
-            cliente = servidor.accept();
-            // entrada DataInputStream
-            // salida DataOutputStream
-            
-            // Canal para leer al cliente
-            entrada = new DataInputStream(cliente.getInputStream());
-            // Canal para enviar (escribir) al cliente
-            salida = new DataOutputStream(cliente.getOutputStream());
+            while(true) {
+                // Establecer conexión
+                cliente = servidor.accept();
+                // entrada DataInputStream
+                // salida DataOutputStream
+
+                // Canal para leer al cliente
+                entrada = new DataInputStream(cliente.getInputStream());
+                // Mostrar lo que manda el cliente
+                System.out.println("Cliente dice >> " + entrada.readUTF());
+                // Datos del cliente
+                String ip = cliente.getInetAddress().getHostAddress();
+                int port = cliente.getPort();
+                System.out.println("Datos: " + ip + " " + port + "\n");
+
+                // Canal para enviar (escribir) al cliente
+                salida = new DataOutputStream(cliente.getOutputStream());
+                // Ahora vamos a enviarle un mensaje al cliente
+                salida.writeUTF("Mensaje recibido +)");
+
+                // Cerrrar conexión del cliente
+                cliente.close();
+            }
         }
         catch (IOException ex) {
             ex.printStackTrace();
