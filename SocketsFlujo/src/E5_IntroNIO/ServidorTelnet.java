622af6d916c0dcf1f9c7f53b2f57a2d251fc726d
David <davbetm@gmail.com>
2019-02-12 09:57:09 -0600

    8: TelnetServer with java.nio

---
+++ SocketsFlujo/src/E5_IntroNIO/ServidorTelnet.java
@@ -0,0 +1,70 @@
+package E5_IntroNIO;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
+/**
+ *
+ * @author david
+ */
+public class ServidorTelnet {
+    private ServerSocketChannel ssc;
+    private SocketChannel cliente;
+    private ByteBuffer buffer; // entrada y salida
+    private int puerto;
+
+    public ServidorTelnet(int puerto) {
+        this.puerto = puerto;
+        try {
+            this.ssc = ServerSocketChannel.open();
+            this.ssc.socket().bind(new InetSocketAddress(this.puerto));
+            // Configurar para que no tenga modo bloqueante
+            this.ssc.configureBlocking(false);
+        }
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public void escuchar() {
+        try {
+            // Enviar mensaje al cliente
+            this.buffer = ByteBuffer.wrap("Mensaje recibido\n".getBytes());
+            System.out.println("Esperando conexiones...");
+            // Aceptar una petición del cliente
+            
+            while (true) {    
+                this.cliente = this.ssc.accept();
+                if(this.cliente == null) Thread.sleep(8000);
+                else {
+                    System.out.println("¡El cliente se ha conectado! " + 
+                    this.ssc.socket().toString());
+                    // Resetear posiciones
+                    this.buffer.rewind();
+                    this.cliente.write(this.buffer);
+                    this.buffer.clear();
+                    this.cliente.read(this.buffer);
+                    this.buffer.flip();
+                    while(this.buffer.hasRemaining()) {
+                        System.out.print((char)this.buffer.get());
+                    }
+                    //Cerramos
+                    this.cliente.close();
+                    break;
+                }
+            }
+            //System.out.println("FIN");
+        }
+        catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String args[]) {
+        ServidorTelnet st = new ServidorTelnet(4500);
+        st.escuchar();
+    }
+}
